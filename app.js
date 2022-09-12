50e244da3f297f60dbbb801aa7a6cd1ff04f5a8a
JasonCMLo <jason.chun.man.lo@gmail.com>
2022-09-12 15:13:58 -0400

    Add user field, shift to daily log

--- app.js
+++ app.js
@@ -1,76 +1,104 @@
 const { default: mongoose } = require("mongoose");
 
-
-require('dotenv').config()
-
-
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoKey = process.env.MongoAPI
+const mongoKey = process.env.MongoAPI;
+const mongoID = process.env;
 
-mongoose.connect("mongodb+srv://Jason:"+mongoKey+"@cluster0.tstbsbq.mongodb.net/boWell")
+mongoose.connect(
+  "mongodb+srv://Jason_Desktop:" +
+    mongoKey +
+    "@cluster0.tstbsbq.mongodb.net/boWell"
+);
 
 const MealSchema = {
-    name: [String],
-    source: String,
-    ingredients: [String],
-}
+  name: String,
+  source: String,
+  ingredients: [String],
+};
 
 const bmSchema = {
-    rating: Number,
-}
+  rating: Number,
+};
 
 const Meal = mongoose.model("meal", MealSchema);
 
 const BM = mongoose.model("bm", bmSchema);
 
 const DaySchema = {
-    day: Date,
-    meals: [MealSchema],
-    bms: [bmSchema]
-}
+  day: Date,
+  meals: [MealSchema],
+  bms: [bmSchema],
+  user: { type: String, required: true },
+};
 
 const Log = mongoose.model("Log", DaySchema);
 
-app.get("/", function(req, res) {
-    res.render("main");
-})
+app.get("/", function (req, res) {
+  res.render("main");
+});
 
-app.post("/meal", function(req, res) {
-    const input = req.body;
+app.post("/:postType", function (req, res) {
+  const input = req.body;
 
-    let newMeal = new Meal({
-        name: input.name.split(),
+  console.log(input);
+
+  const today = new Date();
+
+  const options = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+
+  const currDate = today.toLocaleDateString("en-US", options);
+
+  const postType = req.params.postType;
+
+  Log.findOne({ day: currDate }, function (err, results) {
+    if (!results) {
+      const mdb_date = new Log({
+        day: currDate,
+        user: input.user,
+      });
+      mdb_date.save();
+    }
+
+    if (postType === "meal") {
+      let newMeal = new Meal({
+        name: input.name,
         source: input.source,
         ingredients: input.ingredients.split(),
-    })
-    
-    newMeal.save();
+      });
+      console.log(newMeal);
 
-    console.log(newMeal);
+      results.meals.push(newMeal);
+      results.save();
 
-    res.redirect("/");
-})
+      res.redirect("/");
+    }
 
-app.post("/bm", function(req, res) {
-    const input = req.body;
+    if (postType === "bm") {
+      let newBM = new BM({
+        rating: input.bmRating,
+      });
 
-    let newBM = new BM({
-        rating: input.bmRating
-    })
+      results.bms.push(newBM);
+      results.save();
 
-    newBM.save();
+      res.redirect("/");
+    }
+  });
+});
 
-    res.redirect("/");
-})
-
-app.listen(3000, function() {
-    console.log("Server running on port 3000");
-})
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
